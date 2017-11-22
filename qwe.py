bc8763c4cac623ec3659bbe4310d16751493c90d
Руслан <600244145g@gmail.com>
2017-11-22 10:57:15 +0500

    v_1

--- qwe.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-connect = psycopg2.connect(database='eao', user='postgres', host='localhost', password='54-71394')
-cursor = connect.cursor()
-name = input('name ')
-about = input('about ')
-cursor.execute('insert into goods values(default, 4, 250.36 , 25 ,\'' + name + '\', ' + '\'' + about + '\');')
